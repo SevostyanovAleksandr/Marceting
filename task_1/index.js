a2b43b3565da88d2f719cf239156d203aabaecde
G-Shamkhal <g_shamkhal_2001@mail.ru>
2024-05-02 00:43:53 +0500

    добавлена функция

--- task_1/index.js
+++ task_1/index.js
@@ -5,4 +5,20 @@ console.log(encoded, translations)
 
 
 
-// console.log(decoded)
+// const mappedData = data.map (item => {
+// const mappeditem ={};
+
+let mappedData = encoded.reduce((acc, item) => {
+let mappedItem= {};
+for (let key in item){
+if(translations [item[key]]) {
+mappedItem[key] =translations[item[key]];
+}else {
+mappedItem[key]=item[key];
+}
+}
+acc.push(mappedItem);
+return acc;
+},[]
+);
+console.log(mappedData)
